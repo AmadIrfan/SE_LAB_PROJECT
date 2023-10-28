e3f5184eac45da714709d78a716c129118a165f9
Amad <amadirfan443@gmail.com>
2023-10-28 22:21:54 +0500

    new packages

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ require("dotenv").config();
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-
+const cors = require("cors");
 var cookieParser = require("cookie-parser");
-
 var logger = require("morgan");
 
 var app = express();
@@ -12,18 +11,17 @@ var app = express();
 const db = require("./utils/db/mongoDB");
 const morgan = require("morgan");
 
-app.use(morgan('dev'));
-
+app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-	res.send('this is server')
+	res.send("this is server");
 });
 
-
-app.listen(3000,()=>{
-console.log('server is running');
-})
+app.listen(3000, () => {
+	console.log("server is running");
+});
