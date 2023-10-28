ed5c9d0562ada3d41703b5a421595917be85158f
Amad <amadirfan443@gmail.com>
2023-10-28 22:19:57 +0500

    server folder deleted

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ var logger = require("morgan");
 var app = express();
 
 const db = require("./utils/db/mongoDB");
+const morgan = require("morgan");
 
+app.use(morgan('dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,18 +23,7 @@ app.get("/", (req, res) => {
 	res.send('this is server')
 });
 
-app.use(function (req, res, next) {
-	next(createError(404));
-});
 
 app.listen(3000,()=>{
 console.log('server is running');
 })
-app.use(function (err, req, res, next) {
-	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
-	res.status(err.status || 500);
-	res.render("error");
-});
-
-module.exports = app;
